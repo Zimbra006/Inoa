5e02f65bcbf8a66b0c7ea7921f7e43b45d6a09f5
Guilherme Amazonas Böhme <guibohme@gmail.com>
2024-08-09 17:33:12 -0300

    Commit inicial com o projeto em branco.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+    Programa feito para o processo seletivo da Inoa Sistemas.
+    Objetivo do programa: 
+    - Enviar um alerta, via email, para avisar sobre o preço de uma ação.
+    - O nome da ação e os limites de compra/venda serão passados como parâmetros
+      na linha de comando.
+    - Os emails a serem alertados serão lidos em um arquivo de texto.
+
+    TODO:
+    - Ler os parâmetros na linha de comando
+    - Obter o valor da ação em tempo real
+    - Ler os emails a serem avisados
+    - Enviar os emails caso o valor ultrapasse um dos limites
+
+*/
+
+using System;
+
+namespace Inoa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(args.Length);
+        }
+    }
+}
